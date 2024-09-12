d43dc1da4cf4799aebb283c7fcf4c1a70d65b4f6
konstantin-paulus <mail@konstantinpaulus.de>
2024-09-12 18:28:08 +0200

    removed browser support flag

--- showcase/src/borwser-support.ts
+++ showcase/src/borwser-support.ts
@@ -1,9 +1,1 @@
-const isSupported = !navigator.userAgent.toLowerCase().includes('firefox');
-
-
-if (!isSupported) {
-  const el = document.querySelector('[id="browser-not-supported"]') as HTMLParagraphElement;
-  el.style.display = 'block';
-}
-
 export const isMobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
